fd8f2ac535d45c6f73f1f120a9d3238cba021a39
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2021-05-10 17:39:17 +0530

    Add files via upload

---
+++ C#/ConsoleApp-Bigger-Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_Bigger_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Greater integer is ");
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.Write(b);
+            }
+        }
+    }
+}
